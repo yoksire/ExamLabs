c627e619ad4f119f6b94e5671765964039b5f0b9
yoksire <79806563+yoksire@users.noreply.github.com>
2023-10-15 19:22:58 +0530

    added QuestionAdapter and question_item_layout.xml

--- app/src/main/java/com/example/exampractice/QuestionAdapter.kt
+++ app/src/main/java/com/example/exampractice/QuestionAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.exampractice
 
-class QuestionAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class QuestionAdapter(private var questionList:MutableList<QuestionModel>) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionAdapter.ViewHolder {
+        val myView:View= LayoutInflater.from(parent.context).inflate(R.layout.question_item_layout,parent,false)
+        return ViewHolder(myView)
+    }
+
+    override fun onBindViewHolder(holder: QuestionAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
 }
